9685f880ca8f1e12d2aa5125269883126368a2c2
Joshy <josh.ludahl@gmail.com>
2021-07-30 18:11:31 -0700

    dependencies update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,13 +47,29 @@ dependencies {
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
+
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.activity:activity-compose:1.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+
+    // Compose
+    implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
+    implementation("androidx.compose.ui:ui-tooling:c")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
+
+    implementation("androidx.compose.foundation:foundation:${rootProject.extra["compose_version"]}")
+    // Material Design
+    implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
+    // Material design icons
+    implementation("androidx.compose.material:material-icons-core:${rootProject.extra["compose_version"]}")
+    implementation("androidx.compose.material:material-icons-extended:${rootProject.extra["compose_version"]}")
+    // Integration with observables
+    implementation("androidx.compose.runtime:runtime-livedata:${rootProject.extra["compose_version"]}")
+    implementation("androidx.compose.runtime:runtime-rxjava2:${rootProject.extra["compose_version"]}")
+
+
+
 }
