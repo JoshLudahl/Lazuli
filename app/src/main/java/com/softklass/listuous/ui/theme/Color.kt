4329d8e910ce720af7b6ea6e70bf26476fdb1e0e
Joshy <josh.ludahl@gmail.com>
2021-11-05 22:20:11 -0700

    Updated prefed coax stream

--- app/src/main/java/com/softklass/listuous/ui/theme/Color.kt
+++ app/src/main/java/com/softklass/listuous/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Peach = Color(red = 254, green = 241, blue = 230)
+val LightOrange = Color(red = 249, green = 213, blue = 167)
+val Orange = Color(red = 255, green = 176, blue = 133)
+val Blue = Color(red = 144, green = 170, blue = 203)
