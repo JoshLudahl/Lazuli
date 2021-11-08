bbd5e60b1283c1fe82a492c4de7d6d80abcd8944
Joshy <josh.ludahl@gmail.com>
2021-11-07 19:52:03 -0800

    Cramuand, swoud alphla, con suy muy ala funta

--- app/src/main/java/com/softklass/listuous/ui/theme/Color.kt
+++ app/src/main/java/com/softklass/listuous/ui/theme/Color.kt
@@ -7,7 +7,14 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
+// Potential color pallet one
 val Gray = Color(red = 157, green = 157, blue = 157)
 val Tan = Color(red = 248, green = 240, blue = 223)
 val Cream = Color(red = 254, green = 251, blue = 243)
 val Blue = Color(red = 121, green = 180, blue = 183)
+
+// Potential color pallet two
+val Black = Color(27, 38, 44)
+val DarkBlue = Color(15, 76, 117)
+val CleanBlue = Color(50, 130, 184)
+val LightBlue = Color(187, 225, 250)
