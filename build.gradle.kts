e5b636c7ca994a5f79ffdd3e293f59764e1c89e3
Joshy <josh.ludahl@gmail.com>
2022-01-29 00:25:26 -0800

    Urada onkip

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.0-alpha03")
+        classpath("com.android.tools.build:gradle:7.2.0-beta01")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
     }
 }
