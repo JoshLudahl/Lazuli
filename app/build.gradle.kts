8123f95181fc4e0b35361c0a7d01825bda77f5ec
joshy <josh.ludahl@gmail.com>
2024-05-16 20:44:40 -0700

    chore: Updated dependency management and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,35 +53,33 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.compose.ui:ui:1.5.4")
-    implementation("androidx.compose.material:material:1.5.4")
-    implementation("androidx.compose.ui:ui-tooling:1.5.4")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.material)
+    implementation(libs.androidx.ui.tooling)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.activity.ktx)
 
-    testImplementation("junit:junit:4.13.2")
+    testImplementation(libs.junit)
 
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
 
     // DATASTORE
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.datastore:datastore-preferences-core:1.0.0")
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.datastore.preferences.core)
 
     // APPSEARCH
-    val appsearch_version = "1.1.0-alpha03"
-
-    implementation("androidx.appsearch:appsearch:$appsearch_version")
+    implementation(libs.androidx.appsearch)
     // Use kapt instead of annotationProcessor if writing Kotlin classes
-    annotationProcessor("androidx.appsearch:appsearch-compiler:$appsearch_version")
+    annotationProcessor(libs.androidx.appsearch.compiler)
 
-    implementation("androidx.appsearch:appsearch-local-storage:$appsearch_version")
+    implementation(libs.androidx.appsearch.local.storage)
     // PlatformStorage is compatible with Android 12+ devices, and offers additional features
     // to LocalStorage.
-    implementation("androidx.appsearch:appsearch-platform-storage:$appsearch_version")
+    implementation(libs.androidx.appsearch.platform.storage)
 }
