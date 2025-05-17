41129e216a3cdf4871b253026145a4b0997cc0b9
joshy <josh.ludahl@gmail.com>
2025-05-17 15:38:25 -0700

    Add FAB to Main screen and update code style settings

--- app/src/main/java/com/softklass/lazuli/ui/main/Main.kt
+++ app/src/main/java/com/softklass/lazuli/ui/main/Main.kt
@@ -1,9 +1,16 @@
 package com.softklass.lazuli.ui.main
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.softklass.lazuli.data.models.Parent
 
@@ -12,11 +19,21 @@ fun Main(
     viewModel: MainViewModel,
     onDetailItemClick: (String) -> Unit
 ) {
-    MainContent(onDetailItemClick, listOf())
+    Scaffold(
+        floatingActionButton = { FabButton(onClick = { /*TODO*/ }) },
+        floatingActionButtonPosition = androidx.compose.material3.FabPosition.End
+    ) { innerPadding ->
+        MainContent(
+            modifier = Modifier.padding(innerPadding),
+            onDetailItemClick,
+            listOf()
+        )
+    }
 }
 
 @Composable
 fun MainContent(
+    modifier: Modifier = Modifier,
     onDetailItemClick: (String) -> Unit,
     list: List<Parent>
 ) {
@@ -58,4 +75,13 @@ fun EmptyList() {
 @Composable
 fun EmptyListPreview() {
     EmptyList()
+}
+
+@Composable
+fun FabButton(onClick: () -> Unit) {
+    ExtendedFloatingActionButton(
+        onClick = { onClick() },
+        icon = { Icon(Icons.Filled.Add, "Add list button.") },
+        text = { Text(text = "Add List") },
+    )
 }
