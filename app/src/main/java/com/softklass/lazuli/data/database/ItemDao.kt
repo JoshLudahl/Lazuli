e92adc0bbdc5a4e95f0934bbf03a328c374188bb
joshy <josh.ludahl@gmail.com>
2025-05-17 20:49:09 -0700

    feat: Implemented methods for getting and adding items.

--- app/src/main/java/com/softklass/lazuli/data/database/ItemDao.kt
+++ app/src/main/java/com/softklass/lazuli/data/database/ItemDao.kt
@@ -2,18 +2,21 @@ package com.softklass.lazuli.data.database
 
 import androidx.room.Dao
 import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Upsert
 import com.softklass.lazuli.data.models.Item
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ItemDao {
 
     @Query("SELECT * FROM list_item WHERE parent = :parent")
-    fun getAll(parent: Int): List<Item>
+    fun getAll(parent: Int): Flow<List<Item>>
 
-    @Upsert
-    fun upsertAll(vararg items: Item)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addItem(vararg items: Item)
 
     @Delete
     fun delete(item: Item)
--- app/src/main/java/com/softklass/lazuli/data/repository/ItemRepository.kt
+++ app/src/main/java/com/softklass/lazuli/data/repository/ItemRepository.kt
@@ -1,10 +1,13 @@
 package com.softklass.lazuli.data.repository
 
 import com.softklass.lazuli.data.database.ItemDao
+import com.softklass.lazuli.data.models.Item
 import javax.inject.Inject
 
 class ItemRepository @Inject constructor(
     private val itemDao: ItemDao
 ) {
+    fun getAllItems(parent: Int) = itemDao.getAll(parent)
 
+    suspend fun addItem(item: Item) = itemDao.addItem(item)
 }
