542aafcb59ab6f02678d819bd4326feadc63cab3
joshy <josh.ludahl@gmail.com>
2025-05-19 21:08:42 -0700

    Remove unused Spacer import from ListDetailScreen.kt
    The Spacer import was not being utilized in the code and has been removed to clean up the file. This change improves code readability and reduces unnecessary imports.

--- app/src/main/java/com/softklass/lazuli/ui/detail/ListDetailScreen.kt
+++ app/src/main/java/com/softklass/lazuli/ui/detail/ListDetailScreen.kt
@@ -2,7 +2,6 @@ package com.softklass.lazuli.ui.detail
 
 import android.util.Log
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
