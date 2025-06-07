d52d27c166aae491b4900e9158abb09edfbdd605
joshy <josh.ludahl@gmail.com>
2025-06-06 20:23:46 -0700

    chore: Removed unused imports
    - Removed `Icons`, `ArrowBack`, and `IconButton` imports from `HeaderUi.kt`.

--- app/src/main/java/com/softklass/lazuli/ui/list/HeaderUi.kt
+++ app/src/main/java/com/softklass/lazuli/ui/list/HeaderUi.kt
@@ -11,11 +11,8 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
