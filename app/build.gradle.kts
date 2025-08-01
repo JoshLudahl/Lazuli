cfd6c5d02cd98c9717a00ac6cdf88f84da8973c4
joshy <josh.ludahl@gmail.com>
2025-08-01 11:07:44 -0700

    Increment app version to 15 (2025.8.01)
    Updated the `versionCode` to 15 and `versionName` to "2025.8.01" in `app/build.gradle.kts`. This reflects a new build iteration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.softklass.lazuli"
         minSdk = 26
         targetSdk = target
-        versionCode = 14
-        versionName = "2025.7.31"
+        versionCode = 15
+        versionName = "2025.8.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments.putAll(mutableMapOf("clearPackageData" to "true"))
