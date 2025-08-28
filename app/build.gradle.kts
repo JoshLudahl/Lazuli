b4514d09eb15d4f7124532eb3fd1e381a561afe0
joshy <josh.ludahl@gmail.com>
2025-08-28 14:18:07 -0700

    Increment app version to 33 (2025.8.28)
    Updated the `versionCode` to 33 and `versionName` to "2025.8.28" in `app/build.gradle.kts`. This reflects a new build iteration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.softklass.lazuli"
         minSdk = 26
         targetSdk = target
-        versionCode = 32
-        versionName = "2025.8.27"
+        versionCode = 33
+        versionName = "2025.8.28"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments.putAll(mutableMapOf("clearPackageData" to "true"))
